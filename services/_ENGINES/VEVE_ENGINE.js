43f3bf79a8522328a3ce54c307f86855016bf5c0
LK <lk@alienbuild.uk>
2023-09-27 17:11:48 +0100

    Engine updates (untested)

--- services/_ENGINES/VEVE_ENGINE.js
+++ services/_ENGINES/VEVE_ENGINE.js
@@ -6,10 +6,11 @@ import {getImxTransactions} from "../../queries/getImxTransactions";
 
 const prisma = new PrismaClient()
 
+const VEVE = "de2180a8-4e26-402a-aed1-a09a51e6e33d";
+
 const fetchIMXTransactions = async (nextToken) => {
-    console.log('[INFO] Fetching IMX transactions...')
     try {
-        const response = await fetch(`https://3vkyshzozjep5ciwsh2fvgdxwy.appsync-api.us-west-2.amazonaws.com/graphql`, {
+        const response = await fetch('https://3vkyshzozjep5ciwsh2fvgdxwy.appsync-api.us-west-2.amazonaws.com/graphql', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -18,282 +19,202 @@ const fetchIMXTransactions = async (nextToken) => {
             },
             body: JSON.stringify({
                 query: getImxTransactions(),
-                variables: {
-                    address: "0xa7aefead2f25972d80516628417ac46b3f2604af",
-                    pageSize: 1000, // 2673 is max?
-                    nextToken: nextToken
-                }
+                variables: { address: "0xa7aefead2f25972d80516628417ac46b3f2604af", pageSize: 1000, nextToken }
             })
         });
+
         if (!response.ok) throw new Error(`Unable to fetch IMX transactions. Status: ${response.status}`);
-
-        const imxTransactions = await response.json();
-
-        return imxTransactions.data.listTransactionsV2;
+        const { data: { listTransactionsV2 } } = await response.json();
+        return listTransactionsV2;
     } catch (e) {
-        console.log('[ERROR] Unable to fetch IMX transactions.', e);
+        console.error('[ERROR] Fetch IMX transactions failed:', e);
         throw e;
     }
-}
+};
 
-const processIMXTransactions = async (transactions) => {
-    let VEVE = "de2180a8-4e26-402a-aed1-a09a51e6e33d"
+const processMintTransaction = (transaction, imxMintsArr, imxWalletsArr) => {
+    imxMintsArr.push({
+        id: transaction.txn_id,
+        wallet_id: transaction.transfers[0].to_address,
+        timestamp: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        token_id: Number(transaction.transfers[0].token.token_id)
+    });
+
+    imxWalletsArr.push({
+        id: transaction.transfers[0].to_address,
+        first_activity_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        last_activity_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        active: true
+    });
+};
+
+const processTransferTransaction = (transaction, imxTransArr, imxWalletsArr) => {
+    imxTransArr.push({
+        id: transaction.txn_id,
+        from_wallet: transaction.transfers[0].from_address,
+        to_wallet: transaction.transfers[0].to_address,
+        timestamp: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        token_id: Number(transaction.transfers[0].token.token_id)
+    });
+
+    imxWalletsArr.push({
+        id: transaction.transfers[0].from_address,
+        last_activity_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        active: true,
+        KYC: true
+    });
+
+    imxWalletsArr.push({
+        id: transaction.transfers[0].to_address,
+        first_activity_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        last_activity_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        active: true
+    });
+};
+
+const extractUniqueId = (transaction, imxTokensArr) => {
+    let unique_cover_id = null;
+    let collectible_id = null;
+
+    try {
+        const reComic = /comic_cover\.([a-f\d-]+)\./;
+        const reCollectible = /collectible_type_image\.([a-f\d-]+)\./;
+        const image_url = transaction.transfers[0]?.token?.token_detail?.image_url || null;
+
+        if (image_url && image_url.length > 0) {
+            const comicMatch = image_url.match(reComic);
+            const collectibleMatch = image_url.match(reCollectible);
+
+            if (comicMatch) unique_cover_id = comicMatch[1];
+            if (collectibleMatch) collectible_id = collectibleMatch[1];
+        }
+    } catch (e) {
+        console.error('[ERROR] Unable to extract id from image_url for token:', e);
+    }
+
+    imxTokensArr.push({
+        token_id: Number(transaction.transfers[0].token.token_id),
+        wallet_id: transaction.transfers[0].to_address,
+        txn_type: transaction.txn_type,
+        mint_date: moment.unix(Number(transaction.txn_time) / 1000).utc().format(),
+        unique_cover_id: unique_cover_id || null,
+        collectible_id: collectible_id || null
+    });
+};
+
+const processTransactions = (items) => {
     let imxMintsArr = [];
     let imxTransArr = [];
     let imxWalletsArr = [];
     let imxTokensArr = [];
 
-    for (const transaction of transactions.items) {
-        let timestamp = moment.unix(Number(transaction.txn_time) / 1000).utc().format()
-        let txn_id = transaction.txn_id
-        let txn_type = transaction.txn_type
-        let from_wallet = transaction.transfers[0].from_address
-        let to_wallet = transaction.transfers[0].to_address
-        let token_id = Number(transaction.transfers[0].token.token_id)
-        let image_url = transaction.transfers[0]?.token?.token_detail?.image_url || null;
-        // let name = transaction.transfers[0].token.token_detail.name
+    items.forEach(transaction => {
+        if (transaction.txn_type === "mint") processMintTransaction(transaction, imxMintsArr, imxWalletsArr);
+        else if (transaction.txn_type === "transfer") processTransferTransaction(transaction, imxTransArr, imxWalletsArr);
+        extractUniqueId(transaction, imxTokensArr);
+    });
 
-        let unique_cover_id = null
-        let collectible_id = null
+    return [imxMintsArr, imxTransArr, imxWalletsArr, imxTokensArr];
+};
 
-        try {
-            const reComic = /comic_cover\.([a-f\d-]+)\./;
-            const reCollectible = /collectible_type_image\.([a-f\d-]+)\./;
-
-            if (image_url && image_url.length > 0) {
-                const comicMatch = image_url.match(reComic);
-                const collectibleMatch = image_url.match(reCollectible);
-
-                if (comicMatch) {
-                    unique_cover_id = comicMatch[1];
-                } else if (collectibleMatch) {
-                    collectible_id = collectibleMatch[1];
-                }
-            }
-        } catch (e) {
-            console.log(`[ERROR] Unable to extract id from image_url for token: ${token_id} => `, e);
-        }
-
-        if (txn_type === "mint") {
-            imxMintsArr.push({
-                id: txn_id,
-                wallet_id: to_wallet,
-                timestamp: timestamp,
-                token_id: token_id
-            });
-            imxWalletsArr.push({
-                id: to_wallet,
-                first_activity_date: timestamp,
-                last_activity_date: timestamp,
-                active: true // if wallet is active since 8/1/2022
-            })
-
-        } else if (txn_type === "transfer") {
-            imxTransArr.push({
-                id: txn_id,
-                from_wallet: from_wallet,
-                to_wallet: to_wallet,
-                timestamp: timestamp,
-                token_id: token_id
-            });
-            imxWalletsArr.push({
-                id: from_wallet,
-                last_activity_date: timestamp,
-                active: true, // if wallet is active since 8/1/2022
-                KYC: true // if wallet sold something = Has KYC after ~6/1/2023
-            })
-            imxWalletsArr.push({
-                id: to_wallet,
-                first_activity_date: timestamp,
-                last_activity_date: timestamp,
-                active: true // if wallet is active since 8/1/2022
-            })
-        }
-
-        imxTokensArr.push({
-            token_id: token_id,
-            wallet_id: to_wallet,
-            txn_type: txn_type,
-            mint_date: timestamp,
-            unique_cover_id: unique_cover_id || null,
-            collectible_id: collectible_id || null
-        });
-    }
-
-    const previousMintCount = await prisma.veve_mints.count()
-    const previousTransferCount = await prisma.veve_transfers.count()
-    const previousWalletCount = await prisma.veve_wallets.count()
-    const previousTokenCount = await prisma.veve_tokens.count()
-
+const performUpserts = async (imxMintsArr, imxTransArr, imxWalletsArr, imxTokensArr) => {
     try {
-        await prisma.veve_mints.createMany({
-            data: imxMintsArr,
-            skipDuplicates: true
-        })
-        await prisma.veve_transfers.createMany({
-            data: imxTransArr,
-            skipDuplicates: true
-        })
-        await Promise.all(imxWalletsArr.map(async (wallet) => {
-            // console.log('[INFO] Upserting wallet: ', wallet);
+        await prisma.veve_mints.createMany({ data: imxMintsArr, skipDuplicates: true });
+        await prisma.veve_transfers.createMany({ data: imxTransArr, skipDuplicates: true });
 
-            const existingWallet = await prisma.veve_wallets.findUnique({
-                where: {
-                    id: wallet.id
-                }
-            });
-
-            const upsertParams = {
-                where: {
-                    id: wallet.id
-                },
+        await Promise.all(imxWalletsArr.map(async wallet => {
+            const existingWallet = await prisma.veve_wallets.findUnique({ where: { id: wallet.id } });
+            await prisma.veve_wallets.upsert({
+                where: { id: wallet.id },
                 update: {
-                    last_activity_date: wallet.last_activity_date > existingWallet?.last_activity_date
-                        ? wallet.last_activity_date
-                        : existingWallet?.last_activity_date,
-                    first_activity_date: existingWallet ? existingWallet.first_activity_date : wallet.last_activity_date,
-                    KYC: wallet.KYC,
-                    active: wallet.active // Update other fields as needed
+                    last_activity_date: existingWallet && existingWallet.last_activity_date > wallet.last_activity_date ? existingWallet.last_activity_date : wallet.last_activity_date,
+                    first_activity_date: existingWallet ? existingWallet.first_activity_date : wallet.first_activity_date,
+                    active: wallet.active,
+                    KYC: existingWallet ? existingWallet.KYC || wallet.KYC : wallet.KYC
                 },
-                create: {
-                    ...wallet,
-                    first_activity_date: wallet.last_activity_date
-                }
-            };
-
-            if (existingWallet) {
-                // Update first_activity_date only if it's earlier than what's in the table
-                if (wallet.first_activity_date < existingWallet.first_activity_date) {
-                    upsertParams.update.first_activity_date = wallet.first_activity_date;
-                } else {
-                    delete upsertParams.update.first_activity_date;
-                }
-            }
-
-            await prisma.veve_wallets.upsert(upsertParams);
+                create: wallet
+            });
         }));
-        await Promise.all(imxTokensArr.map(async (token) => {
-            try {
-                // Remove the txn_type field from the token object
-                const { txn_type, ...tokenWithoutTxnType } = token;
-                const existingToken = await prisma.veve_tokens.findFirst({
-                    where: {
-                        token_id: token.token_id
-                    },
-                    orderBy: {
-                        mint_date: 'desc'
-                    }
-                });
 
-                if (existingToken) {
-                    const updateFields = {
-                        wallet_id: token.wallet_id
-                    };
-
-                    if (txn_type === 'mint') {
-                        updateFields.mint_date = token.mint_date;
-                    }
-
-                    // console.log('[INFO] Upserting token: ', token);
-                    await prisma.veve_tokens.upsert({
-                        where: {
-                            token_id: token.token_id
-                        },
-                        update: updateFields,
-                        create: {
-                            token_id: token.token_id,
-                            wallet_id: token.wallet_id,
-                            mint_date: token.mint_date,
-                            unique_cover_id: token.unique_cover_id || null,
-                            collectible_id: token.collectible_id || null
-                        }
-                    });
-                } else {
-                    // console.log('[INFO] Inserting new token: ', token);
-                    await prisma.veve_tokens.create({
-                        data: tokenWithoutTxnType
-                    });
-                }
-            } catch (error) {
-                console.error('[ERROR] Failed to upsert token:', error);
-            }
+        await Promise.all(imxTokensArr.map(async token => {
+            const existingToken = await prisma.veve_tokens.findFirst({
+                where: { token_id: token.token_id },
+                orderBy: { mint_date: 'desc' }
+            });
+            await prisma.veve_tokens.upsert({
+                where: { token_id: token.token_id },
+                update: {
+                    wallet_id: token.wallet_id,
+                    mint_date: existingToken && token.txn_type === 'mint' ? token.mint_date : existingToken.mint_date,
+                    unique_cover_id: token.unique_cover_id || existingToken.unique_cover_id,
+                    collectible_id: token.collectible_id || existingToken.collectible_id
+                },
+                create: token
+            });
         }));
-        const currentMintCount = await prisma.veve_mints.count()
-        let new_mints = currentMintCount - previousMintCount
-        if (currentMintCount >= previousMintCount) {
-            await prisma.imx_stats.update({
-                where: {
-                    project_id: VEVE
-                }, data: {
-                    token_count: currentMintCount
-                }
-            })
-            console.log(`[INFO] ${new_mints} new mints`)
-        }
-
-        const currentTransferCount = await prisma.veve_transfers.count()
-        let new_transfers = currentTransferCount - previousTransferCount
-        if (currentTransferCount >= previousTransferCount) {
-            await prisma.imx_stats.update({
-                where: {
-                    project_id: VEVE
-                }, data: {
-                    transaction_count: currentTransferCount
-                }
-            })
-            console.log(`[INFO] ${new_transfers} new transfers`)
-        }
-
-        const currentWalletCount = await prisma.veve_wallets.count()
-        let new_wallets = currentWalletCount - previousWalletCount
-        if (currentWalletCount >= previousWalletCount) {
-            await prisma.imx_stats.update({
-                where: {
-                    project_id: VEVE
-                }, data: {
-                    wallet_count: currentWalletCount
-                }
-            })
-            console.log(`[INFO] ${new_wallets} new wallets`)
-        }
-
-        const currentTokenCount = await prisma.veve_tokens.count()
-        let new_tokens = currentTokenCount - previousTokenCount
-        if (currentTokenCount >= previousTokenCount) {
-            // const imxStats = await prisma.imx_stats.update({
-            //     where: {
-            //         project_id: VEVE
-            //     }, data: {
-            //         token_count: currentTokenCount
-            //     }
-            // })
-            // await pubsub.publish('IMX_VEVE_TOKEN_STATS_UPDATED', {
-            //     imxVeveStatsUpdated: imxStats
-            // })
-            console.log(`[INFO] ${new_tokens} new tokens`)
-        }
-
     } catch (e) {
-        console.error('[ERROR] Prisma operation failed:', e);
+        console.error('[ERROR] Prisma Upsert Failed:', e);
     }
-}
+};
+
+const updateStats = async (previousMintCount, previousTransferCount, previousWalletCount, previousTokenCount) => {
+    try {
+        const [currentMintCount, currentTransferCount, currentWalletCount, currentTokenCount] = await Promise.all([
+            prisma.veve_mints.count(),
+            prisma.veve_transfers.count(),
+            prisma.veve_wallets.count(),
+            prisma.veve_tokens.count()
+        ]);
+
+        const newMints = currentMintCount - previousMintCount;
+        const newTransfers = currentTransferCount - previousTransferCount;
+        const newWallets = currentWalletCount - previousWalletCount;
+        const newTokens = currentTokenCount - previousTokenCount;
+
+        await prisma.imx_stats.update({
+            where: { project_id: VEVE },
+            data: {
+                token_count: currentTokenCount,
+                transaction_count: currentTransferCount,
+                wallet_count: currentWalletCount,
+                new_mints: newMints,
+            }
+        });
+
+        // Log the new statistics
+        console.log(`[INFO] ${newMints} new mints, ${newTransfers} new transfers, ${newWallets} new wallets, ${newTokens} new tokens`);
+    } catch (e) {
+        console.error('[ERROR] Update Stats Failed:', e);
+    }
+};
+
+const processIMXTransactions = async (transactions) => {
+    try {
+        const previousMintCount = await prisma.veve_mints.count();
+        const previousTransferCount = await prisma.veve_transfers.count();
+        const previousWalletCount = await prisma.veve_wallets.count();
+        const previousTokenCount = await prisma.veve_tokens.count();
+
+        const [imxMintsArr, imxTransArr, imxWalletsArr, imxTokensArr] = processTransactions(transactions.items);
+        await performUpserts(imxMintsArr, imxTransArr, imxWalletsArr, imxTokensArr);
+        await updateStats(previousMintCount, previousTransferCount, previousWalletCount, previousTokenCount);
+    } catch (e) {
+        console.error('[ERROR] Process Transactions Failed:', e);
+    }
+};
 
 export const VEVE_IMX_TRANSACTIONS = async () => {
     try {
         let nextToken = null;
         do {
-            const timestampStart = new Date().toISOString();
             const imxTransactions = await fetchIMXTransactions(nextToken);
             nextToken = imxTransactions.nextToken;
             await processIMXTransactions(imxTransactions);
-            const timestampEnd = new Date().toISOString();
-            console.log(`[START] Loop iteration started at: ${timestampStart}`);
-            console.log(`[END] Loop iteration finished at: ${timestampEnd}`);
         } while (nextToken);
     } catch (e) {
-        console.log(`[ERROR] VEVE_IMX_TRANSACTIONS:`, e);
+        console.error('[ERROR] VEVE_IMX_TRANSACTIONS Failed:', e);
     }
-}
+};
 
 // let metadata
 
