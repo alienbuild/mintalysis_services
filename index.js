98815489b01d223ed5800627500b34ff02c226c7
LK <lk@alienbuild.uk>
2023-10-02 15:38:18 +0100

    stash

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dotenv.config();
 console.log('[ALICE IS WAITING FOR INSTRUCTION]')
 
 import { PrismaClient } from "@prisma/client"
-import {UPDATE_USER_STATUS} from "./services/MINTALYSIS/UPDATE_USER_STATUS";
+import {UPDATE_USER_STATUS} from "./services/MINTALYSIS/UPDATE_USER_STATUS.js";
 
 export const prisma = new PrismaClient()
 
@@ -12,8 +12,6 @@ const main = async () => {
 
     try {
         await prisma.$connect();
-        console.log('Connected to Prisma');
-        console.log('About to call UPDATE_USER_STATUS');
         await UPDATE_USER_STATUS(prisma); // Here prisma instance is passed as argument.
         console.log('UPDATE_USER_STATUS has been called');
     } catch (error) {
--- services/MINTALYSIS/UPDATE_USER_STATUS.js
+++ services/MINTALYSIS/UPDATE_USER_STATUS.js
@@ -1,34 +1,68 @@
+import fetch from 'node-fetch'
+
 export const UPDATE_USER_STATUS = async (prisma) => {
     try {
-        console.log('[UPDATE_USER_STATUS] Function Entered');
-
         const now = new Date();
 
-        console.log('[UPDATE_USER_STATUS] About to update OFFLINE users');
-        await prisma.User.updateMany({
-            where: {
-                last_seen: {
-                    lt: new Date(now - 30 * 60 * 1000) // 30 minutes ago
-                },
-            },
-            data: {status: 'OFFLINE'},
-        });
-        console.log('[UPDATE_USER_STATUS] OFFLINE users updated');
-
-        console.log('[UPDATE_USER_STATUS] About to update IDLE users');
-        await prisma.User.updateMany({
+        // Fetch IDLE Users
+        const idleUsers = await prisma.user.findMany({
             where: {
                 last_seen: {
                     lt: new Date(now - 15 * 60 * 1000), // 15 minutes ago
                     gte: new Date(now - 30 * 60 * 1000) // but less than 30 minutes ago
                 },
-            },
-            data: {status: 'IDLE'},
+                status: 'ONLINE' // Only fetch users who are currently marked as ONLINE
+            }
         });
-        console.log('[UPDATE_USER_STATUS] IDLE users updated');
+
+        // Update IDLE Users' Status and Notify
+        for (const user of idleUsers) {
+            await prisma.user.update({
+                where: { id: user.id },
+                data: { status: 'IDLE' },
+            });
+
+            await sendStatusUpdate(user.id, 'IDLE');
+        }
+
+        // Fetch OFFLINE Users
+        const offlineUsers = await prisma.user.findMany({
+            where: {
+                last_seen: {
+                    lt: new Date(now - 30 * 60 * 1000) // 30 minutes ago
+                },
+                status: { in: ['IDLE', 'ONLINE'] } // Only fetch users who are currently marked as IDLE or ONLINE
+            }
+        });
+
+        // Update OFFLINE Users' Status and Notify
+        for (const user of offlineUsers) {
+            await prisma.user.update({
+                where: { id: user.id },
+                data: { status: 'OFFLINE' },
+            });
+
+            await sendStatusUpdate(user.id, 'OFFLINE');
+        }
 
         console.log('[SUCCESS UPDATED USER STATUSES]');
     } catch (error) {
         console.error('[UPDATE_USER_STATUS] FAILED TO UPDATE USER STATUSES: ', error);
     }
 };
+
+async function sendStatusUpdate(userId, newStatus) {
+    try {
+        const response = await fetch('http://localhost:8001/update-user-status', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ userId, newStatus }),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    } catch (error) {
+        console.error('Failed to send status update:', error);
+    }
+}
